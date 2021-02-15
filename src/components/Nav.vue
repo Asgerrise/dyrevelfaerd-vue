a038a164300ce92213394864ecebfc53ee408168
Asgerrise <Asgerrise@live.dk>
2021-02-15 10:06:50 +0100

    Add nav + hero image

---
+++ src/components/Nav.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav class="nav">
+    <div class="nav__wrapper">
+      <router-link to="/" class="nav__logo">
+        <img class="nav__logo-img" src="@/assets/logo.png" alt="Logo" />
+        <h1 class="nav__logo-text">Foreningen for Dyrevelfærd</h1>
+      </router-link>
+      <ul class="nav__links">
+        <router-link to="/" class="nav__link">Hjem</router-link>
+        <router-link to="/about" class="nav__link">Om os</router-link>
+        <router-link to="/about" class="nav__link">Bliv Frivillig</router-link>
+        <router-link to="/about" class="nav__link">Dyr i nød?</router-link>
+        <router-link to="/about" class="nav__link">Adopter et dyr</router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  padding: 1.5em 0;
+  background-color: white;
+
+  &__wrapper {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+
+    @media screen and(max-width: 1200px) {
+      margin: 0 1em;
+    }
+  }
+  &__logo {
+    display: flex;
+    text-decoration: none;
+    align-items: center;
+
+    &-img {
+      margin-right: 1em;
+      max-width: 40px;
+      max-height: 40px;
+    }
+    &-text {
+      font-size: 1.5em;
+      color: #333;
+      font-weight: 500;
+      white-space: nowrap;
+    }
+  }
+  &__links {
+    display: flex;
+    align-items: center;
+  }
+
+  &__link {
+    text-decoration: none;
+    color: #333;
+    margin-left: 1em;
+    opacity: 0.5;
+    white-space: nowrap;
+  }
+}
+
+.router-link-active {
+  opacity: 1;
+}
+</style>
